e71d2b744701b8d95fc5f03599e1ff60ea126a0f
Brianhulela <101281239+Brianhulela@users.noreply.github.com>
2024-10-01 12:22:16 +0200

    Download the model and run inference on the downloaded image

--- yolo_detect.py
+++ yolo_detect.py
@@ -1,3 +1,4 @@
+from ultralytics import YOLO
 import cv2
 import urllib.request
 
@@ -5,3 +6,9 @@ import urllib.request
 url, filename = ("https://images.unsplash.com/photo-1520919971261-fe0be1461d0b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTV8fHBlb3BsZSUyMGFuZCUyMGNhcnMlMjBpbiUyMHN0cmVldHxlbnwwfHwwfHx8MA%3D%3D", "scene.jpg")
 urllib.request.urlretrieve(url, filename)    # Download image
 image = cv2.imread(filename)
+
+# Load a model
+model = YOLO("yolo11x.pt")  # pretrained YOLO11x model
+
+# Run batched inference on a list of images
+results = model(image)  # return a list of Results objects
