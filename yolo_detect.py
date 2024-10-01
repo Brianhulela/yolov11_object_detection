998fc8a868eb145cfa8dd29b88f8ad9750ec7a54
Brianhulela <101281239+Brianhulela@users.noreply.github.com>
2024-10-01 12:22:33 +0200

    Show the annotated bounding boxes on the original image

--- yolo_detect.py
+++ yolo_detect.py
@@ -12,3 +12,9 @@ model = YOLO("yolo11x.pt")  # pretrained YOLO11x model
 
 # Run batched inference on a list of images
 results = model(image)  # return a list of Results objects
+
+# Process results list
+for result in results:
+    boxes = result.boxes  # Boxes object for bounding box outputs
+    result.show()  # display to screen
+    result.save(filename="result.jpg")  # save to disk
